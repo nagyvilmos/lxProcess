033da70cf5994f9ec6baf02960eb6af94a13298e
William Norman-Walker <william.norman.walker@gmail.com>
2017-09-06 08:45:47 +0100

    Add package info

---
+++ src/lexa/core/process/package-info.java
@@ -0,0 +1,22 @@
+/*==============================================================================
+ *  Lexa - Property of William Norman-Walker
+ * ------------------------------------------------------------------------------
+ *  package-info.java (lxProcess)
+ * ------------------------------------------------------------------------------
+ *  Author:  William Norman-Walker
+ *  Created: August 2017
+ * ==============================================================================
+ */
+
+/**
+ * Framework for handling a a message in the Lexa framework.
+ * <p>
+ * Processes receive massages and then handle the processing of them. They can
+ * request calls to other processes.  When processing messages, the process can
+ * request additional data from other processes.
+ *
+ *
+ * @author  william
+ * @since   2016-08
+ */
+package lexa.core.process;
---
+++ src/lxprocess/package-info.java
@@ -0,0 +1,20 @@
+/*==============================================================================
+ * Lexa - Property of William Norman-Walker
+ *------------------------------------------------------------------------------
+ * package-info.java (lxProcess)
+ *------------------------------------------------------------------------------
+ * Author:  William Norman-Walker
+ * Created: July 2017
+ *==============================================================================
+ */
+
+/**
+ * Test handler for a Lexa Process.
+ * <p>See:</p>
+ * <ul>
+ * <li>{@link lexa.core.process} Lexa Process components</li>
+ * <li>{@link lxprocess.ProcessTest} the launcher for process tests</li>
+ * <li>{@link lxprocess.TestProcess} the handler for process tests</li>
+ * </ul>
+ */
+package lxprocess;
